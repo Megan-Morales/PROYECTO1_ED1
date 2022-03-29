d0fb662ae94f96ddedddcaea58be667e00ded650
EMILIO ANTONIO BARILLAS CONTRERAS <eabarillasco@correo.url.edu.gt>
2022-03-28 18:38:42 -0600

    created project and singleton

---
+++ PROYECTO1_ED1/Helpers/Data.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROYECTO1_ED1.Helpers
+{
+    public class Data
+    {
+        //anti patrón singleton 
+        private static Data _instance = null;
+        public static Data Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Data();
+                }
+                return _instance;
+            }
+        }
+    }
+}
---
+++ PROYECTO1_ED1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PROYECTO1_ED1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
